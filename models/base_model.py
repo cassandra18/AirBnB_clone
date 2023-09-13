c0b6f7f4bad0ca446c616aff082a1c40474fac90
B2ma <beatrice.terer@gmail.com>
2023-09-13 15:30:01 +0000

    Updated models/base_model.py as required

--- models/base_model.py
+++ models/base_model.py
@@ -39,20 +39,31 @@ class BaseModel():
         deserialization process: create a dictionary representation with
         "simple object type" of our BaseModel
     """
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         """
         Initialize a new instance of BaseModel
-        id: string:
-            assign with an uuid when an instance is created:
-        created_at: datetime
-            current datetime when instance is created
-        updated_at: datetime
-            current datetime when instance is created and
-            will be updated on every object change
+
+        If kwargs is not empty, set instance attributes based on its
+            key-value pairs.
+        If kwargs is empty, create a new instance with a unique ID and current
+            datetime.
+
+        Args:
+            *args: Not used.
+            **kwargs: Key-value pairs for initializing instance attributes.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """
@@ -70,8 +81,10 @@ class BaseModel():
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['created_at'] = self.created_at.strftime(
+                '%Y-%m-%dT%H:%M:%S.%f')
+        obj_dict['updated_at'] = self.updated_at.strftime(
+                '%Y-%m-%dT%H:%M:%S.%f')
         return obj_dict
 
     def __str__(self):

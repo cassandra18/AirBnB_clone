82a2654565c46c408c73a7377f792b3c4276b15d
B2ma <beatrice.terer@gmail.com>
2023-09-15 07:52:16 +0000

    Created task 9 modules

---
+++ models/review.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Class representing a review.
+
+    Public class attributes:
+    - place_id (str): The ID of the place associated with the review.
+    - user_id (str): The ID of the user who wrote the review.
+    - text (str): The text content of the review.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a new instance of Review.
+
+        Args:
+            *args: Not used.
+            **kwargs: Key-value pairs for initializing instance attributes.
+        """
+        super().__init__(*args, **kwargs)
+        self.place_id = ""
+        self.user_id = ""
+        self.text = ""
+
+    def __str__(self):
+        """
+        Return a human-readable string representation of the Review instance.
+
+        Returns:
+            str: A string containing the class name, ID, and attribute
+            dictionary.
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
---
+++ models/state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""State class model"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Class representing a state.
+
+    Public class attributes:
+    - name (str): The name of the state.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a new instance of State.
+
+        Args:
+            *args: Not used.
+            **kwargs: Key-value pairs for initializing instance attributes.
+        """
+        super().__init__(*args, **kwargs)
+        self.name = ""
+
+    def __str__(self):
+        """
+        Return a human-readable string representation of the State instance.
+
+        Returns:
+            str: A string containing the class name, ID, and attribute
+            dictionary.
+        """
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
